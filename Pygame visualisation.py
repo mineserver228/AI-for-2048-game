5ddad8ae18dab2cea5f3720672fe1c49a1a07cf5
Roma <romashkaviktorovich@gmail.com>
2023-02-21 16:52:07 +0300

    Fixed bug with the crash of the game when the entire field is filled. Added numbers in cells (powers of twos). Added score. Added random coloring of cells.

--- Pygame visualisation.py
+++ Pygame visualisation.py
@@ -2,10 +2,13 @@ import pygame
 import Game_math_functions as gm
 import keyboard
 
+pygame.font.init()
 # pygame settings
 background_colour = (201, 194, 109)
-screen = pygame.display.set_mode((gm.x * 110 + 10, gm.y * 110 + 10))
+screen = pygame.display.set_mode((gm.x * 110 + 200, gm.y * 110 - 10))
 pygame.display.set_caption("2048 game with AI")
+font = pygame.font.SysFont("arial", 36)
+font_score = pygame.font.SysFont("arial", 25)
 # Variables
 mass = []
 mass = gm.mass_fill(mass)
@@ -13,7 +16,8 @@ key_flagd = 0
 key_flagu = 0
 key_flagl = 0
 key_flagr = 0
-
+# initialization of cell colors
+gm.colours = gm.colours_init(gm.colours)
 # loop
 running = True
 while running:
@@ -58,12 +62,22 @@ while running:
     if not keyboard.is_pressed("right"):
         if key_flagr == 1:
             key_flagr = 0
-
     # Draw sqares with numbers
     for i in range(gm.x):
         for g in range(gm.y):
             # colours not working
+            colour = 0
+            division = mass[g][i]
+            while not division < 2:
+                colour += 1
+                division = division // 2
             pygame.draw.rect(screen,
-                        (255, 255 - mass[g][i] * 3, 255 - mass[g][i] * 3),
-                        (110 * i, 110 * g, 100, 100))
+                (gm.colours[colour * 3 + 1], gm.colours[colour * 3 + 2], gm.colours[colour * 3 + 3]),
+                (110 * i, 110 * g, 100, 100))
+            if not mass[g][i] == 0:
+                render = font.render(f"{mass[g][i]}", True, (0, 0, 0))
+                screen.blit(render, (110 * i + 30, 110 * g + 30))
+            render = font_score.render(f"Your score:{gm.score}", True, (0, 0, 0))
+            screen.blit(render, (gm.x * 110 + 3, 20))
+
     pygame.display.update()
