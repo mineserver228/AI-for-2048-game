8efb6adfcd2c7de93e2a9a441c5ecaa9bf77ccdc
Roma <romashkaviktorovich@gmail.com>
2023-02-21 16:53:32 +0300

    Fixed bug with the crash of the game when the entire field is filled. Added numbers in cells (powers of twos). Added score. Added random coloring of cells.

--- Game_math_functions.py
+++ Game_math_functions.py
@@ -1,8 +1,9 @@
 import random
 
+score = 0
 x = 5
 y = 5
-
+colours = []
 
 # filling game desk at the start
 def mass_fill(array):
@@ -23,6 +24,7 @@ def mass_fill(array):
 
 # colculating, when you click key "right"
 def key_right(array):
+    global score
     for i in range(x - 1):
         for g in range(y):
             if array[g][i + 1] == 0:
@@ -31,11 +33,13 @@ def key_right(array):
             elif array[g][i + 1] == array[g][i]:
                 array[g][i + 1] *= 2
                 array[g][i] = 0
+                score += array[g][i + 1]
     return array
 
 
 # colculating, when you click key "left"
 def key_left(array):
+    global score
     for i in range(x - 1):
         for g in range(y):
             if array[g][x - 2 - i] == 0:
@@ -44,11 +48,13 @@ def key_left(array):
             elif array[g][x - 2 - i] == array[g][x - 1 - i]:
                 array[g][x - 2 - i] *= 2
                 array[g][x - 1 - i] = 0
+                score += array[g][x - 2 - i]
     return array
 
 
 # colculating, when you click key "dawn"
 def key_down(array):
+    global score
     for i in range(y - 1):
         for g in range(x):
             if array[i + 1][g] == 0:
@@ -57,11 +63,13 @@ def key_down(array):
             elif array[i + 1][g] == array[i][g]:
                 array[i + 1][g] *= 2
                 array[i][g] = 0
+                score += array[i + 1][g]
     return array
 
 
 # colculating, when you click key "up"
 def key_up(array):
+    global score
     for i in range(y - 1):
         for g in range(x):
             if array[y - 2 - i][g] == 0:
@@ -70,19 +78,34 @@ def key_up(array):
             elif array[y - 2 - i][g] == array[y - 1 - i][g]:
                 array[y - 2 - i][g] *= 2
                 array[y - 1 - i][g] = 0
+                score += array[y - 2 - i][g]
     return array
 
 
 def add_blocks(array):
+    global score
     for i in range(random.randint(1, 2)):
         randx = random.randint(0, x - 1)
         randy = random.randint(0, y - 1)
-        while not array[randy][randx] == 0:
+        i = 0
+        while not (array[randy][randx] == 0 or i > 10000):
+            i += 1
             randx = random.randint(0, x - 1)
             randy = random.randint(0, y - 1)
         if random.randint(0, 1) == 0:
             array[randy][randx] = 2
         else:
             array[randy][randx] = 4
+        if i > 10000:
+            array = []
+            array = mass_fill(array)
+            score = 0
     return array
 
+
+def colours_init(colours):
+    for i in range(1000):
+        colours.append(random.randint(0, 255))
+        colours.append(random.randint(0, 255))
+        colours.append(random.randint(0, 255))
+    return colours
