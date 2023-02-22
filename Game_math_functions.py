96652136561832e24e4ada0f0c69a16335c9e2df
Roma <romashkaviktorovich@gmail.com>
2023-02-22 12:18:33 +0300

    Added autoclicker function. Fixed bugs with blocks moving. Enchanted interface. Added "Best score"(without saving). You can stop autoclicker, when you press key "space".

--- Game_math_functions.py
+++ Game_math_functions.py
@@ -5,35 +5,19 @@ x = 5
 y = 5
 colours = []
 
-# filling game desk at the start
-def mass_fill(array):
-    m = []
-    for i in range(x):
-        m.append(0)
-    for i in range(y):
-        array.append(m.copy())
-    if random.randint(0, 1) == 0:
-        array[random.randint(0, x - 1)][random.randint(0, y - 1)] = 4
-    elif random.randint(0, 1) == 0:
-        array[random.randint(0, x - 1)][random.randint(0, y - 1)] = 2
-    else:
-        array[random.randint(0, x - 1)][random.randint(0, y - 1)] = 2
-        array[random.randint(0, x - 1)][random.randint(0, y - 1)] = 2
-    return array
-
-
 # colculating, when you click key "right"
 def key_right(array):
     global score
     for i in range(x - 1):
-        for g in range(y):
-            if array[g][i + 1] == 0:
-                array[g][i + 1] = array[g][i]
-                array[g][i] = 0
-            elif array[g][i + 1] == array[g][i]:
-                array[g][i + 1] *= 2
-                array[g][i] = 0
-                score += array[g][i + 1]
+        for i in range(x - 1):
+            for g in range(y):
+                if array[g][x - i - 1] == 0:
+                    array[g][x - i - 1] = array[g][x - i - 2]
+                    array[g][x - i - 2] = 0
+                elif array[g][x - i - 1] == array[g][x - i - 2]:
+                    array[g][x - i - 1] *= 2
+                    array[g][x - i - 2] = 0
+                    score += array[g][i + 1]
     return array
 
 
@@ -41,14 +25,15 @@ def key_right(array):
 def key_left(array):
     global score
     for i in range(x - 1):
-        for g in range(y):
-            if array[g][x - 2 - i] == 0:
-                array[g][x - 2 - i] = array[g][x - 1 - i]
-                array[g][x - 1 - i] = 0
-            elif array[g][x - 2 - i] == array[g][x - 1 - i]:
-                array[g][x - 2 - i] *= 2
-                array[g][x - 1 - i] = 0
-                score += array[g][x - 2 - i]
+        for i in range(x - 1):
+            for g in range(y):
+                if array[g][i] == 0:
+                    array[g][i] = array[g][i + 1]
+                    array[g][i + 1] = 0
+                elif array[g][i] == array[g][i + 1]:
+                    array[g][i] *= 2
+                    array[g][i + 1] = 0
+                    score += array[g][i]
     return array
 
 
@@ -56,14 +41,15 @@ def key_left(array):
 def key_down(array):
     global score
     for i in range(y - 1):
-        for g in range(x):
-            if array[i + 1][g] == 0:
-                array[i + 1][g] = array[i][g]
-                array[i][g] = 0
-            elif array[i + 1][g] == array[i][g]:
-                array[i + 1][g] *= 2
-                array[i][g] = 0
-                score += array[i + 1][g]
+        for i in range(y - 1):
+            for g in range(x):
+                if array[i + 1][g] == 0:
+                    array[i + 1][g] = array[i][g]
+                    array[i][g] = 0
+                elif array[i + 1][g] == array[i][g]:
+                    array[i + 1][g] *= 2
+                    array[i][g] = 0
+                    score += array[i + 1][g]
     return array
 
 
@@ -71,35 +57,35 @@ def key_down(array):
 def key_up(array):
     global score
     for i in range(y - 1):
-        for g in range(x):
-            if array[y - 2 - i][g] == 0:
-                array[y - 2 - i][g] = array[y - 1 - i][g]
-                array[y - 1 - i][g] = 0
-            elif array[y - 2 - i][g] == array[y - 1 - i][g]:
-                array[y - 2 - i][g] *= 2
-                array[y - 1 - i][g] = 0
-                score += array[y - 2 - i][g]
+        for i in range(y - 1):
+            for g in range(x):
+                if array[y - 2 - i][g] == 0:
+                    array[y - 2 - i][g] = array[y - 1 - i][g]
+                    array[y - 1 - i][g] = 0
+                elif array[y - 2 - i][g] == array[y - 1 - i][g]:
+                    array[y - 2 - i][g] *= 2
+                    array[y - 1 - i][g] = 0
+                    score += array[y - 2 - i][g]
     return array
 
 
 def add_blocks(array):
     global score
-    for i in range(random.randint(1, 2)):
+    randx = random.randint(0, x - 1)
+    randy = random.randint(0, y - 1)
+    i = 0
+    while not (array[randy][randx] == 0 or i > 10000):
+        i += 1
         randx = random.randint(0, x - 1)
         randy = random.randint(0, y - 1)
-        i = 0
-        while not (array[randy][randx] == 0 or i > 10000):
-            i += 1
-            randx = random.randint(0, x - 1)
-            randy = random.randint(0, y - 1)
-        if random.randint(0, 1) == 0:
-            array[randy][randx] = 2
-        else:
-            array[randy][randx] = 4
-        if i > 10000:
-            array = []
-            array = mass_fill(array)
-            score = 0
+    if random.randint(1, 10) == 1:
+        array[randy][randx] = 4
+    else:
+        array[randy][randx] = 2
+    if i > 10000:
+        array = [[0] * x for i in range(y)]
+        array = add_blocks(array)
+        score = 0
     return array
 
 
--- Pygame_visualisation.py
+++ Pygame_visualisation.py
@@ -1,3 +1,5 @@
+import random
+import time
 import pygame
 import Game_math_functions as gm
 import keyboard
@@ -5,17 +7,19 @@ import keyboard
 pygame.font.init()
 # pygame settings
 background_colour = (201, 194, 109)
-screen = pygame.display.set_mode((gm.x * 110 + 200, gm.y * 110 - 10))
+screen = pygame.display.set_mode((gm.x * 110 + 300, gm.y * 110 - 10))
 pygame.display.set_caption("2048 game with AI")
 font = pygame.font.SysFont("arial", 36)
 font_score = pygame.font.SysFont("arial", 25)
 # Variables
-mass = []
-mass = gm.mass_fill(mass)
 key_flagd = 0
 key_flagu = 0
 key_flagl = 0
 key_flagr = 0
+mass = [[0] * gm.x for i in range(gm.y)]
+mass = gm.add_blocks(mass)
+max_score = 0
+auto_click = False
 # initialization of cell colors
 gm.colours = gm.colours_init(gm.colours)
 # loop
@@ -62,6 +66,19 @@ while running:
     if not keyboard.is_pressed("right"):
         if key_flagr == 1:
             key_flagr = 0
+    if auto_click:
+        if not keyboard.is_pressed("space"):
+            rand = random.randint(1, 4)
+            if rand == 1:
+                mass = gm.key_down(mass)
+            if rand == 2:
+                mass = gm.key_up(mass)
+            if rand == 3:
+                mass = gm.key_left(mass)
+            if rand == 4:
+                mass = gm.key_right(mass)
+            mass = gm.add_blocks(mass)
+
     # Draw sqares with numbers
     for i in range(gm.x):
         for g in range(gm.y):
@@ -77,7 +94,11 @@ while running:
             if not mass[g][i] == 0:
                 render = font.render(f"{mass[g][i]}", True, (0, 0, 0))
                 screen.blit(render, (110 * i + 30, 110 * g + 30))
-            render = font_score.render(f"Your score:{gm.score}", True, (0, 0, 0))
-            screen.blit(render, (gm.x * 110 + 3, 20))
+            render = font_score.render(f"Score:{gm.score}", True, (0, 0, 0))
+            screen.blit(render, (gm.x * 110 + 3, 5))
+            if max_score < gm.score:
+                max_score = gm.score
+            render = font_score.render(f"Best score:{max_score}", True, (0, 0, 0))
+            screen.blit(render, (gm.x * 110 + 3, 30))
 
     pygame.display.update()
